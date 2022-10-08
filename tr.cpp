f6dfb529519320141fd13d23800ca8b28532279e
thejayeshrane <68503856+thejayeshrane@users.noreply.github.com>
2022-10-08 21:45:16 +0530

    Created tr file
    Trail for working or not check

---
+++ tr.cpp
@@ -0,0 +1,17 @@
+#include <iostream> //for C++ code
+#include <stdio.h> //for C code
+#include <stdlib.h> //for using malloc function
+#include <string> // used for string class, here working without it also
+using namespace std; //for C++ code
+
+int main()
+{
+   
+  int n;
+  cin>>n;
+  cout<<n;
+
+
+
+  return 0;
+}
